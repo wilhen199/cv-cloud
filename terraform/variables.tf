e91af5c06c80af7d92ff6058915a95aaf9af0550
Wilhen Figueredo <wilhen199@gmail.com>
2026-06-17 22:59:35 -0500

    feat: migrtación exitosa a otra cuenta de AWS

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,5 +1,5 @@
 variable "bucket_name" {
   type        = string
-  default     = "wf-cv-digital"
+  default     = "wf-cv-digital-prod"
   description = "Bucket que almacena website de CV digital"
 }
